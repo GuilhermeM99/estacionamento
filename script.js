275714e3b25c9c024273bdaf00ca6fb6a06329dd
GuilhermeM99 <Guilherme.guimpl@hotmail.com>
2026-09-03 13:23:50 -0300

    Fix vehicle registration and display logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let veiculos = JSON.parse(localStorage.getItem("veiculos")) || [];
 for (let i = 0; i <veiculos.length; i++) {
-    veiculos[i][1] = new Data(veiculos[i][1];
+    veiculos[i][1] = new Date(veiculos[i][1]);
 }
 console.log(veiculos);
 
